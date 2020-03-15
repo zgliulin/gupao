e6cac98fdf4fbba977a102ad15277e7df887d1a2
zgliulin <larry_mall@163.com>
2020-03-15 15:55:34 +0800

    20200315 责任链模式和建造者模式结合应用

--- src/com/liulin/study/designpatterns/e_proxymode/dynamicproxy/cglibgroxy/CglibMeipo.java
+++ src/com/liulin/study/designpatterns/e_proxymode/dynamicproxy/cglibgroxy/CglibMeipo.java
@@ -1,12 +1,10 @@
 package com.liulin.study.designpatterns.e_proxymode.dynamicproxy.cglibgroxy;
-import com.liulin.study.designpatterns.e_proxymode.dynamicproxy.jdkproxy.IPerson;
+
 import net.sf.cglib.proxy.Enhancer;
 import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
 
-import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
 
 /**
  * Create by DbL on 2020/3/1
